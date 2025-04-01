3dec135193e9758c31b018e092aef4cbe9a22833
X51Chilz <petermares55@gmail.com>
2025-04-01 11:47:53 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+app.use('/public', express.static('public'));
+
 require('dotenv').config();
 const express = require('express');
 const app = express();
 const whatsappRoutes = require('./routes/whatsapp');
 const { initializeWhatsApp } = require('./services/whatsappClient');
 
-app.use('/public', express.static('public'));
+
 app.use(express.json());
 app.use('/api', whatsappRoutes);
 
